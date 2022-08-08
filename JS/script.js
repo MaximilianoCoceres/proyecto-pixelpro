fc3a4282a84db05f52c2de639e80b92d9cac9f43
MaximilianoCoceres <maximilianococeres7@gmail.com>
2022-08-08 13:41:04 +0200

    agrego contacto

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,20 @@
+const $form = document.querySelector('#form')
+const $button = document.querySelector('#mail_to_form')
+
+
 
 window.addEventListener("scroll", function () {
     var header = this.document.querySelector('header');
     header.classList.toggle("abajo", window.scrollY > 0);
 
 })
+
+
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    $button.setAttribute('href', `mailto:maxiprog7@gmail.com?subject=${form.get('name')} ${form.get('mail')} &body=${form.get('comentario')}`)
+    $button.click()
+}
