6ef456bd792fb96cf6fe96c74e76a82bf1eeeb9d
MaximilianoCoceres <maximilianococeres7@gmail.com>
2023-03-02 22:52:01 +0100

    agrego json para proyectos, y emailjs

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,4 @@
-const $form = document.querySelector('#form')
-const $button = document.querySelector('#mail_to_form')
+const btn = document.getElementById('button');
 
 
 
@@ -10,11 +9,60 @@ window.addEventListener("scroll", function () {
 })
 
 
-$form.addEventListener('submit', handleSubmit)
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
 
-function handleSubmit(event) {
-    event.preventDefault()
-    const form = new FormData(this)
-    $button.setAttribute('href', `mailto:maxiprog7@gmail.com?subject=${form.get('name')} ${form.get('mail')} &body=${form.get('comentario')}`)
-    $button.click()
-}
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_p4s5tfr';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Gracias! Te responderé pronto!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
+fetch("/proyectos.json")
+    .then((res)=>res.json())
+    .then((data)=>{
+        console.log(data)
+        let portfolio = document.querySelector(".row-container");
+        let proyecto = '';
+        
+        for(var i in data.proyectos){
+            let tecnologias='';
+            for(var j in data.proyectos[i].tech){
+                tecnologias +=` <i class="${data.proyectos[i].tech[j]}"></i>`;
+            }
+
+            proyecto += `<div class="column">
+            <figure>
+              <img src=${data.proyectos[i].img} alt=${data.proyectos[i].alt} />
+              <figcaption>
+                <div>
+                  <h2>${data.proyectos[i].title}</h2>
+                  <div class="icons">
+                    ${tecnologias}
+                  </div>
+                  <div>
+                    <a href=${data.proyectos[i].proyecto}
+                      >Ver proyecto</a
+                    >
+                    <a href=${data.proyectos[i].github}>
+                      <i class="fa-brands fa-github-alt"></i
+                    ></a>
+                  </div>
+                </div>
+              </figcaption>
+            </figure>
+          </div> `;
+        }
+
+        portfolio.innerHTML = proyecto;
+    })
